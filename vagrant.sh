6f595069b202367b682f2d702f83c553a0625560
Eremeew Dmitry <littlewolfgm@gmail.com>
2016-11-10 12:53:31 +0500

    up

--- vagrant.sh
+++ vagrant.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+apt-get update
+apt-get install -y Nginx
+apt-get install -y gunicorn
+apt-get install -y python-pip
+pip install Django==1.6.1
+
 ln -fs /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 /etc/init.d/nginx restart
 
