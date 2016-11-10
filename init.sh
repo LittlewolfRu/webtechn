400be54064dd4c37fbb98e8b24574d7e87c7a13c
LittlewolfRu <littlewolfgm@gmail.com>
2016-11-10 12:37:56 +0500

    Update init.sh

--- init.sh
+++ init.sh
@@ -3,5 +3,8 @@
 ln -fs /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 /etc/init.d/nginx restart
 
+git config --global user.email "littlewolgm@gmail.com"                              
+git config --global user.name "littlewolfRu"
+
 gunicorn -c /home/box/web/etc/hello.py hello:app --daemon
 gunicorn -c /home/box/web/etc/django.py wsgi --daemon
