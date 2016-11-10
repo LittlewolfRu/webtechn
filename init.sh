716fb141d8c8ea41b8f3bdde190c6e4b5e1e5810
LittlewolfRu <littlewolfgm@gmail.com>
2016-11-10 12:24:40 +0500

    Update init.sh

--- init.sh
+++ init.sh
@@ -6,5 +6,7 @@ sudo /etc/init.d/nginx restart
 
 git config --global user.email "littlewolgm@gmail.com"                              
 git config --global user.name "littlewolfRu"
+gunicorn -c /home/box/web/etc/hello.py hello:app --daemon
+gunicorn -c /home/box/web/etc/django.py wsgi --daemon
 #sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 #sudo /etc/init.d/gunicorn restart
